62c3b1fe12255d87852dbac4be15a7c6096b571c
limyeonsang <limquo@gmail.com>
2022-06-20 22:59:37 +0900

    Add: constant

---
+++ src/constant/index.ts
@@ -0,0 +1,38 @@
+export const Jung = [
+    {
+        id: 1,
+        title: "title1",
+        emotion: "emotion1",
+        content: "content1"
+    },
+    {
+        id: 2,
+        title: "title2",
+        emotion: "emotion2",
+        content: "content2"
+    },
+    {
+        id: 3,
+        title: "title3",
+        emotion: "emotion3",
+        content: "content3"
+    },
+    {
+        id: 4,
+        title: "title4",
+        emotion: "emotion4",
+        content: "content4"
+    },
+    {
+        id: 5,
+        title: "title5",
+        emotion: "emotion5",
+        content: "content5"
+    },
+    {
+        id: 6,
+        title: "title6",
+        emotion: "emotion6",
+        content: "content6"
+    },
+]
