a9f126bf280c9cb6f9b94ae58b2be23031a5f884
limyeonsang <limquo@gmail.com>
2022-06-20 22:07:53 +0900

    Add: switch styl

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import Switch from "react-switch";
+import * as S from "./styles"
+
 const MainContainer = () => {
-    return <div>main</div>
+    const [onOff, setOnOff] = useState<boolean>(false)
+
+    return (
+        <S.MainContainer>
+            <S.SwitchContainer>
+                <Switch 
+                    onChange={() => setOnOff(onOff => !onOff)} 
+                    checked={onOff}
+                    width={100} 
+                    height={63.5}
+                    onColor="#ffffff"
+                    offColor="#ffffff"
+                    onHandleColor="#FBF799"
+                    offHandleColor="#B060E0"/>
+            </S.SwitchContainer>
+            <div>{onOff}</div>
+        </S.MainContainer>
+    )
 }
 
 export default MainContainer
---
+++ src/containers/Main/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const MainContainer = styled.section`
+    display: flex;
+    justify-content: center;
+`
+
+export const SwitchContainer = styled.div`
+    margin-top: 40px;
+`
