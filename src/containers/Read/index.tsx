1eb1d29eabd5c74f263e41fabf334ef536b07dd4
limyeonsang <limquo@gmail.com>
2022-06-23 19:56:15 +0900

    Add: style

--- src/containers/Read/index.tsx
+++ src/containers/Read/index.tsx
@@ -1,15 +1,22 @@
 import { useRecoilValue } from "recoil"
+import Editor from "../../components/Editor"
 import { jungState } from "../../utils/recoil"
+import * as S from "./styles"
 
 const ReadContainer = () => {
     const jung = useRecoilValue(jungState)
 
     return (
-        <div>
-            <div>{jung.title}</div>
-            <div>{jung.emotion}</div>
-            <div>{jung.content}</div>
-        </div>
+        <S.ReadContainer>
+            <S.Inner>
+                <S.Title>{jung.title}</S.Title>
+                <S.Emotion>{jung.emotion}</S.Emotion>
+                <Editor
+                    value={jung.content}
+                    readOnly={true}
+                />
+            </S.Inner>
+        </S.ReadContainer>
     )
 }
 
--- src/containers/Read/styles.ts
+++ src/containers/Read/styles.ts
@@ -1,4 +1,24 @@
 import styled from "@emotion/styled";
 
 export const ReadContainer = styled.section`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+`
+
+export const Inner = styled.div`
+    margin-top: 100px;
+    width: 50%;
+`
+
+export const Title = styled.div`
+    font-size: 50px;
+    padding-left: 20px;
+`
+
+export const Emotion = styled.div`
+    font-size: 30px;
+    padding-left: 20px;
+    margin-top: 30px;
+    margin-bottom: 50px;
 `
