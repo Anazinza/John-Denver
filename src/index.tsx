ae80d93f0f7ae3eaf7631d5150e3a8f61b021604
limyeonsang <limquo@gmail.com>
2022-07-14 16:36:54 +0900

    Add apollo setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  uri: "https://spot-emo.herokuapp.com/",
+  cache: new InMemoryCache(),
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
